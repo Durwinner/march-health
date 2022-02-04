2319a7c44e2e3d6cc9829a19cc994e4341d3eec3
Ahmad Ghasemi <ahmadghasemi29@outlook.com>
2022-02-04 15:47:31 +0330

    update api login and generate jwt

--- config/plugins.js
+++ config/plugins.js
@@ -3,10 +3,22 @@ module.exports = ({ env }) => ({
     config: {
       provider: "strapi-provider-email-sendinblue",
       providerOptions: {
-        sendinblue_api_key: env("SIB_API_KEY"),
-        sendinblue_default_replyto: env("SIB_DEFAULT_REPLY_TO"),
-        sendinblue_default_from: env("SIB_DEFAULT_FROM"),
-        sendinblue_default_from_name: env("SIB_DEFAULT_FROM_NAME"),
+        sendinblue_api_key: env(
+          "SIB_API_KEY",
+          "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"
+        ),
+        sendinblue_default_replyto: env(
+          "SIB_DEFAULT_REPLY_TO",
+          "ahmadghasemi1403@gmail.com"
+        ),
+        sendinblue_default_from: env(
+          "SIB_DEFAULT_FROM",
+          "ahmadghasemi1403@gmail.com"
+        ),
+        sendinblue_default_from_name: env(
+          "SIB_DEFAULT_FROM_NAME",
+          "teststrapi"
+        ),
       },
     },
   },
--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -8,24 +8,47 @@ module.exports = async (plugin) => {
       const code = Math.floor(10000 + Math.random() * 90000);
 
       console.log(1, code);
-      try {
-        //         const user = yield User.findOne({email});
+      // try {
+      //   //         const user = yield User.findOne({email});
 
-        // console.log(user);
-        const emailOptions = {
-          to: email,
-          subject: "code login",
-          html: `<h1>${code}</h1>`,
-        };
-        await strapi.plugins["email"].services.email.send(emailOptions);
-        console.log(`Email sent to ${email}`);
-      } catch (err) {
-        strapi.log.error(`Error sending email to ${email}`, err);
+      //   // console.log(user);
+      //   const emailOptions = {
+      //     to: email,
+      //     subject: "code login",
+      //     html: `<h1>${code}</h1>`,
+      //   };
+      //   await strapi.plugins["email"].services.email.send(emailOptions);
+      //   console.log(`Email sent to ${email}`);
+      // } catch (err) {
+      //   strapi.log.error(`Error sending email to ${email}`, err);
+      // }
+
+      const user = await strapi
+        .query("plugin::users-permissions.user")
+        .findOne({
+          where: {
+            email: email,
+          },
+        });
+
+      if (!user) {
+        return ctx.badRequest("User does not exist");
       }
 
-      // await strapi.plugins[
-      //   "users-permissions"
-      // ].services.user.sendConfirmationEmail(user);
+      const validPassword = await strapi.plugins[
+        "users-permissions"
+      ].services.user.validatePassword(password, user.password);
+
+      //console.log("validPassword", validPassword);
+      if (!validPassword) {
+        return ctx.badRequest("Old password does not match.");
+      }
+
+      // Update user confirm code
+      await strapi
+        .query("plugin::users-permissions.user")
+        .update({ id: user.id }, { confirmCode: code.toString() });
+
       console.log("code", code);
 
       // console.log(email, password);
@@ -51,8 +74,46 @@ module.exports = async (plugin) => {
   });
 
   //login
-  plugin.controllers.auth.login = (ctx) => {
-    console.log("login");
+  plugin.controllers.auth.login = async (ctx) => {
+    try {
+      const { email, password, confirmCode } = ctx?.request?.body;
+
+      const user = await strapi
+        .query("plugin::users-permissions.user")
+        .findOne({
+          where: {
+            email: email,
+          },
+        });
+      if (user.confirmCode !== confirmCode) {
+      }
+
+      if (!user) {
+        return ctx.badRequest("User does not exist");
+      }
+
+      const validPassword = await strapi.plugins[
+        "users-permissions"
+      ].services.user.validatePassword(password, user.password);
+
+      //console.log("validPassword", validPassword);
+      if (!validPassword) {
+        return ctx.badRequest("Old password does not match.");
+      }
+
+      const data = {
+        success: true,
+        token: strapi.plugins[("user", "users-permissions")].services.jwt.issue(
+          {
+            id: user.id,
+          }
+        ),
+      };
+      ctx.send(data);
+    } catch (error) {
+      console.log("login");
+      ctx.badRequest("Something went wrong", { error });
+    }
   };
 
   plugin.routes["content-api"].routes.push({
